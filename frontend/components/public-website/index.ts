565755f1cc9849a0a538a56d9ecff23a934a7e98
Rubayit Alam <147439986+rubayitalam@users.noreply.github.com>
2026-02-10 22:49:47 +0600

    Delete frontend directory

--- frontend/components/public-website/index.ts
+++
@@ -1,5 +0,0 @@
-export { HeroSection } from './hero-section';
-export { GallerySection } from './gallery-section';
-export { AboutSection } from './about-section';
-export { ContactSection } from './contact-section';
-export { PublicWebsiteRenderer } from './public-website-renderer';
